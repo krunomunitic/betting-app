281bae1e70b0610d373e92f94c1baa4cbd5bb720
krunomunitic <kmunitic@gmail.com>
2020-10-20 01:53:34 +0200

    Update design (very simple), add ticket creation, placing bets

--- clientapp/src/components/Fixtures.vue
+++ clientapp/src/components/Fixtures.vue
@@ -1,23 +1,39 @@
 <template>
     <div>
         <div v-for='fixtureCompetition in fixturesByCompetition' :key='fixtureCompetition.competitionName'>
-            <div>{{fixtureCompetition.competitionName}}</div>
+            <b-row align-h="between">
+                <b-col cols="5">
+                    <div>{{fixtureCompetition.competitionName}}</div>
+                </b-col>
+                <b-col cols="7">
+                </b-col>
+            </b-row>
             <div v-for="fixture in fixtureCompetition.fixtures" :key='fixture.id'>
-                {{fixture.data}} {{fixture.homeTeamName}} - {{fixture.awayTeamName}} {{fixture.result}}
-                <b-button-group v-for="odds in fixture.odds" :key="odds.name">
-                    <b-button v-if="odds.name === '1'">{{odds.value}}</b-button>
-                    <b-button v-if="odds.name === '2'">{{odds.value}}</b-button>
-                    <b-button v-if="odds.name === 'X'">{{odds.value}}</b-button>
-                    <b-button v-if="odds.name === '12'">{{odds.value}}</b-button>
-                    <b-button v-if="odds.name === 'X1'">{{odds.value}}</b-button>
-                    <b-button v-if="odds.name === 'X2'">{{odds.value}}</b-button>
-                </b-button-group>
+                <b-row align-h="between">
+                    <b-col cols="5">
+                        {{fixture.data}} {{fixture.homeTeamName}} - {{fixture.awayTeamName}} {{fixture.result}}
+                    </b-col>
+                    <b-col cols="7">
+                        <b-row>
+                            <div v-for="odds in fixture.odds" :key="odds.name">
+                                <b-button v-if="odds.name === '1'" @click="addBet(fixture.id, odds)">{{odds.value}}</b-button>
+                                <b-button v-if="odds.name === '2'" @click="addBet(fixture.id, odds)" >{{odds.value}}</b-button>
+                                <b-button v-if="odds.name === 'X'" @click="addBet(fixture.id, odds)">{{odds.value}}</b-button>
+                                <b-button v-if="odds.name === '12'" @click="addBet(fixture.id, odds)">{{odds.value}}</b-button>
+                                <b-button v-if="odds.name === 'X1'" @click="addBet(fixture.id, odds)">{{odds.value}}</b-button>
+                                <b-button v-if="odds.name === 'X2'" @click="addBet(fixture.id, odds)">{{odds.value}}</b-button>
+                            </div>
+                        </b-row>
+                    </b-col>
+                </b-row>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+    // import { mapActions } from 'vuex'
+
     export default {
         data() {
             return {};
@@ -29,6 +45,13 @@
             fixturesByCompetition() {
                 return this.$store.state.fixturesByCompetition
             },
+        },
+        methods: {
+            addBet(fixtureId, odds) {
+                this.$store.dispatch('addBet', { fixtureId: fixtureId, odds })
+                // TODO: extra bets on this fixture are disabled
+            }
+            // ...mapActions(['addBet', './store/index']),
         }
     }
 </script>
--- clientapp/src/store/index.js
+++ clientapp/src/store/index.js
@@ -8,6 +8,7 @@ export default new Vuex.Store({
     state: {
         competitionsBySports: [],
         fixturesByCompetition: [],
+        ticket: {},
     },
     getters: {
         competitionsBySports: state => {
@@ -16,6 +17,9 @@ export default new Vuex.Store({
         fixturesByCompetition: state => {
             return state.fixturesByCompetition
         },
+        getTicket(state) {
+            return state.ticket;
+        },
     },
     mutations: {
         SET_COMPETITIONBYSPORTS(state, competitionsBySports) {
@@ -23,6 +27,9 @@ export default new Vuex.Store({
         },
         SET_FIXTURESBYCOMPETITION(state, fixturesByCompetition) {
             state.fixturesByCompetition = fixturesByCompetition
+        },
+        SET_TICKET(state, ticket) {
+            state.ticket = ticket
         },
     },
     actions: {
@@ -36,5 +43,18 @@ export default new Vuex.Store({
                 commit('SET_FIXTURESBYCOMPETITION', data)
             })
         },
+        addBet({ commit, getters }, bet) {
+            const ticket = getters.getTicket
+
+            // TODO: Validate if bet can be placed on this ticket
+            if (ticket.bets && ticket.bets.length) {
+                ticket.bets.push(bet)
+            }
+            else {
+                ticket.bets = [bet]
+            }
+
+            commit('SET_TICKET', ticket)
+        }
     },
 })
